80d8ec579ac8efa2b3827e27bffc0e34c2947788
Ryan Dantzler <ryandantzler@gmail.com>
2023-02-15 07:14:01 -0800

    Cleaned up game clock display logic

--- nfl.js
+++ nfl.js
@@ -44,19 +44,22 @@ $(document).ready(function() {
 	var updateView = function() {
 		superBowl = stats.events[0].competitions[0];
 
-		var timeRemaning = superBowl.status.displayClock == undefined ? "0:00" : superBowl.status.displayClock,
-			homeTeam = superBowl.competitors[0],
+		var homeTeam = superBowl.competitors[0],
 			awayTeam = superBowl.competitors[1],
 			lastNumHome = parseInt(homeTeam.score % 10),
 			lastNumAway = parseInt(awayTeam.score % 10),
-			quarter = superBowl.status.period > 4 || superBowl.status.type.state == "post" ? superBowl.status.type.description.toUpperCase() : "Q" + superBowl.status.period;
+			quarter = "Q" + superBowl.status.period + " " + superBowl.status.displayClock;
 
-			$('.play_nums div:not(.col_0)').each(function() {
-				if (!isNaN($(this).text())) {
-					$(this).css({'color':'green', 'font-weight':'bold', 'background':'lightgreen'});
-					openCount++;
-				}
-			});
+		if (superBowl.status.type.description.toUpperCase() == "HALFTIME" || superBowl.status.type.state == "post") {
+			quarter = superBowl.status.type.description.toUpperCase();
+		}
+
+		$('.play_nums div:not(.col_0)').each(function() {
+			if (!isNaN($(this).text())) {
+				$(this).css({'color':'green', 'font-weight':'bold', 'background':'lightgreen'});
+				openCount++;
+			}
+		});
 
 		$('#home-score').text(homeTeam.score);
 		$('#away-score').text(awayTeam.score);
@@ -66,22 +69,13 @@ $(document).ready(function() {
 		else
 			$('#info').text("#SHOWMETHEMONEY").css({'color':'', 'font-style':'italic'});
 
-		if (quarter.indexOf("P") > -1)
-			$('#game-clock').text("Q1 15:00");
-		else {
-
-			if (quarter[0] == "F")
-				$('#game-clock').text("FINAL");
-			else if (quarter[0] == "H")
-				$('#game-clock').text("HALFTIME");
-			else
-				$('#game-clock').text(quarter + " " + timeRemaning);
+		if (superBowl.status.type.state !== "pre") {
+			$('#game-clock').text(quarter);
 
 			switch (quarter) {
 				case "Q2":
 					getWinners(parseInt(homeTeam.linescores[0].value), parseInt(awayTeam.linescores[0].value));
 					break;
-				case "H":
 				case "HALFTIME":
 				case "Q3":
 					getWinners(parseInt(homeTeam.linescores[0].value), parseInt(awayTeam.linescores[0].value));
@@ -93,10 +87,8 @@ $(document).ready(function() {
 					getWinners(parseInt(homeTeam.linescores[0].value) + parseInt(homeTeam.linescores[1].value), parseInt(awayTeam.linescores[0].value) + parseInt(awayTeam.linescores[1].value));
 					getWinners(parseInt(homeTeam.linescores[0].value) + parseInt(homeTeam.linescores[1].value) + parseInt(homeTeam.linescores[2].value), parseInt(awayTeam.linescores[0].value) + parseInt(awayTeam.linescores[1].value) + parseInt(awayTeam.linescores[2].value));
 					break;
-				case "F":
 				case "FINAL":
 				case "FINAL/OT":
-				case "FO":
 					getWinners(parseInt(homeTeam.linescores[0].value), parseInt(awayTeam.linescores[0].value));
 					getWinners(parseInt(homeTeam.linescores[0].value) + parseInt(homeTeam.linescores[1].value), parseInt(awayTeam.linescores[0].value) + parseInt(awayTeam.linescores[1].value));
 					getWinners(parseInt(homeTeam.linescores[0].value) + parseInt(homeTeam.linescores[1].value) + parseInt(homeTeam.linescores[2].value), parseInt(awayTeam.linescores[0].value) + parseInt(awayTeam.linescores[1].value) + parseInt(awayTeam.linescores[2].value));
